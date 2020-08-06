9023a418a704e9a3688b0f96712e1fe072c0724a
Madison McCardle <madison.mccardle@powerhrg.com>
2020-08-06 11:49:28 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react'
+
+// lists a series of movie reviews on the page
+// presentation component: stateless and functional.
+const MovieReviews = ({reviews}) => {
+  return(
+    // top-level element with the class review-list
+      // each review is wrapped by an element with class review
+    <ul className='review-list'>
+      {reviews.map(review => <li className='review' key={review.display_title}> {review.display_title} </li>)}
+    </ul>
+  )
+}
+
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,35 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+// provide a searchable interface allowing the user to enter a search term and then receive a list of reviews that match the search term(s)
+export default class SearchableMovieReviewsContainer extends Component {
+  state = {
+    reviews: [],
+    searchTerm: ''
+  }
+
+
+  handleChange = (event) =>{
+    this.setState({searchTerm: event.target.value})
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault()
+    fetch(URL + `&query=${this.state.searchTerm}`)
+    .then(resp => resp.json())
+    .then(data => this.setState({reviews: data.results}))
+  }
+
+  // top-level wrapping element with class searchable-movie-reviews
+  render() {
+    return (
+      <div className='searchable-movie-reviews'>
+        <form onSubmit={this.submitHandler}>
+          <input onChange={this.handleChange} type="text" placeholder="search for movie review" value={this.state.searchTerm}></input>
+          <button type="submit">search</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews}></MovieReviews>
+      </div>
+    )
+  }
+}
